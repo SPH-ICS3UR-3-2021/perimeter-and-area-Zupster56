93dc5cf851638a9a69b8d9f0268bcfdbad0c8cca
clinton.oyedeji <>
2021-05-19 16:13:43 +0000

    am having a problem can you help me

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,32 @@ package main;
  */
 public class Main {
     public static void main(String args[]){
-        
+      // write a program which can calculate the primeter or area of a circle or a rectangle.
+      Scanner in = new Scanner(System.in);
+      int choice=0;
+      // Loop until choice ==5
+      do{
+          //ask for 1-5 
+          System.out.println("Pick from 1-5");
+          choice = in.nextInt();
+          //If 1
+          if (choice==1){
+              // ask for length, width, given perimeter
+              System.out.println("Give me width");
+              int width=in.nextInt();
+              int perimeter = length*2+width*2;
+              System.out.println("The perimeter is *+perimeter");
+          }else if (choice==2){
+          //if 2
+              //ask for lenght, width, given area 
+          }  
+          //if 3
+              // ask for radius, given the perimeter 
+          //if 4
+              // ask radius, given the area 
+          // if 5
+             // quite 
+      }while (choice!=5);
     }
     
 }
